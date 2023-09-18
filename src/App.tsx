24de9d3fe2cd5113ce3ff821a321f743c7c1b62a
Airly12000 <littlefrancis28@gmail.com>
2023-09-18 19:56:20 +0000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='postition-relative bg-color col-dark-text vh-100'>
+			<Navbar />
+			<div className='d-flex flex-column-reverse position-absolute bottom-0 vh-100 socials text-center'>
+				<div className=''>|</div>
+				<div className=''>|</div>
+				<div className=''>|</div>
+				<div className=''>|</div>
+				<div className=''>|</div>
+				<div className='mb-4'>
+					<i className='bi bi-instagram'></i>
+				</div>
+				<div className=''>
+					<i className='bi bi-linkedin'></i>
+				</div>
+				<div className=''>
+					<i className='bi bi-twitter'></i>
+				</div>
+				<div className=''>
+					<i className='bi bi-github'></i>
+				</div>
+			</div>
+			<div className='d-flex flex-column-reverse position-absolute bottom-0 email vh-100 text-center'>
+				<div className=''>|</div>
+				<div className=''>|</div>
+				<div className=''>|</div>
+				<div className=''>|</div>
+				<div className=''>|</div>
+				<div className='rot-90'>littlefrancis28@gmail.com</div>
+			</div>
+			<div className='container pt-100'>
+				<div className='col'>Hello</div>
+				<div className='col'>Hello</div>
+				<div className='col'>Hello</div>
+			</div>
+			<nav className='d-flex'>Copyright &copy;</nav>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+
+function Navbar() {
+	return (
+		<nav className='navbar fixed-top d-flex px-2 px-md-5 py-3 bg-color text-white'>
+			<div className='flex-grow-1'>
+				<img src='assets/img/logo.svg' alt='Logo' height={50} width={50} />
+			</div>
+			<div className='d-none d-md-flex flex-direction-row align-items-center text-center col-dark-text'>
+				<div className='link-width mono'>
+					<span className='col-green'>01.</span> About
+				</div>
+				<div className='link-width mono'>
+					<span className='col-green'>02.</span> Experience
+				</div>
+				<div className='link-width mono'>
+					<span className='col-green'>03.</span> Work
+				</div>
+				<div className='link-width mono'>
+					<span className='col-green'>04.</span> Contact
+				</div>
+				<div className='link-width mono'>
+					<button className='btn btn-outline-success'>Resume</button>
+				</div>
+			</div>
+			<button
+				className='d-block d-md-none border border-0 bg-transparent'
+				type='button'
+				data-bs-toggle='offcanvas'
+				data-bs-target='#offcanvasNavbar'
+				aria-controls='offcanvasNavbar'
+				aria-label='Toggle navigation'>
+				{/* <span className='navbar-toggler-icon'></span> */}
+				<img
+					src='assets/img/toggler.svg'
+					alt='Toggler'
+					height={50}
+					width={50}
+				/>
+			</button>
+			<div
+				className='offcanvas offcanvas-end'
+				tabIndex={-1}
+				id='offcanvasNavbar'
+				aria-labelledby='offcanvasNavbarLabel'>
+				<div className='offcanvas-header d-flex'>
+					<div className='flex-grow-1'></div>
+					<button
+						type='button'
+						className='btn-close'
+						data-bs-dismiss='offcanvas'
+						aria-label='Close'></button>
+				</div>
+				<div className='offcanvas-body text-center'>
+					<ul className='navbar-nav justify-content-end flex-grow-1 pe-3'>
+						<li className='nav-item'>
+							<a className='nav-link active' aria-current='page' href='#'>
+								Home
+							</a>
+						</li>
+						<li className='nav-item'>
+							<a className='nav-link' href='#'>
+								Link
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	);
+}
+
+export default Navbar;
