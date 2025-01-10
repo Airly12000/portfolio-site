311471517463dd20e1c3afb084baeb191dd71314
Francis Agbesi-Ntibrey Junior <58563741+Airly12000@users.noreply.github.com>
2025-01-10 19:40:54 +0000

    Update constants.ts

--- src/sections/About/constants.ts
+++ src/sections/About/constants.ts
@@ -1,6 +1,6 @@
 export const AboutData = {
   paragraphs: [
-    "Results-driven Software (Fullstack) Developer with 3+ years of experience specializing in Frontend Web Development. First Class honors graduate in Computer Science and Engineering, combining technical expertise with a keen eye for design and details.",
+    "Results-driven Fullstack Developer - Frontend Heavy, with over 4 years of experience crafting dynamic, user-focused software solutions. First Class honors graduate in Computer Science and Engineering, combining technical expertise with a keen eye for design and details.",
     "Demonstrated success in developing various software solutions across multiple industries, from mobile apps to enterprise-level platforms. Excels in multitasking, time management, adaptability, and leadership roles within Agile teams. Proven track record of optimizing performance and enhancing usability in complex projects. Committed to fostering effective teamwork, delivering high-quality software solutions that drive business efficiency and user satisfaction.",
   ],
   technologies: [
